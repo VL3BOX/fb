cf0ae4cb28e08422366ae7bcd0ca94aa3b363d3e
浮烟 <rx6@qq.com>
2020-05-14 15:56:22 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+        <Header></Header>
+        <Breadcrumb name="频道名称" slug="slug" root="/slug" publishEnable="true">
+            <img slot="logo" svg-inline src="./assets/img/logo.svg" />
+            <Info />
+        </Breadcrumb>
+        <LeftSidebar>
+            <Nav />
+        </LeftSidebar>
+        <Main :withoutRight="false">
+            primary content
+            <!-- <Comment post-id="100"/> -->
+            <RightSidebar>
+                <Extend/>
+            </RightSidebar>
+            <Footer></Footer>
+        </Main>
+    </div>
+</template>
+
+<script>
+import Info from '@/components/Info.vue';
+import Nav from '@/components/Nav.vue';
+import Extend from '@/components/Extend.vue';
+
+    export default {
+        name : 'App',
+        props:[],
+        data : function(){
+            return {
+                
+            }
+        },
+        computed:{},
+        methods:{},
+        mounted:function(){},
+        components:{
+            Info,
+            Nav,
+            Extend,
+        }
+    }
+</script>
---
+++ src/components/Info.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="m-info">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Info",
+    props: [],
+    data: function() {
+        return {};
+    },
+    computed: {},
+    methods: {},
+    mounted: function() {}
+};
+</script>
+
+<style lang="less">
+</style>

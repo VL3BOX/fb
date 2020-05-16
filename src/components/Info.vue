71c0d7168a225d723d6c6147234816fe6a90714e
iRuxu <rx6@qq.com>
2020-05-16 18:54:24 +0800

    侧边栏搜索过滤功能

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="m-info">
-        
+        最新副本开荒攻略、副本成就攻略、NPC元数据、技能解包，你要的一切都在这里，骨灰PVE党的家园!
     </div>
 </template>
 
@@ -18,4 +18,9 @@ export default {
 </script>
 
 <style lang="less">
+.m-info{
+    .nobreak;
+    overflow-x:auto;
+    .fz(13px);
+}
 </style>

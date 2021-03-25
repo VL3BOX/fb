b78ffe97ce634daec603d93621640cbe320c9c29
iRuxu <rx6@qq.com>
2021-03-25 22:11:18 +0800

    op color

--- src/components/skill_item_call.vue
+++ src/components/skill_item_call.vue
@@ -7,18 +7,21 @@
         <span>(</span>
         <span class="u-call-arg" v-for="(arg, i) in call.args" :key="i">
             <template v-if="arg.type == 'CallExpression'">
-                <em><span class="u-fn">{{ arg.base.name }}</span>(<args :args="arg.arguments"/>)</em>
+                <em
+                    ><span class="u-fn">{{ arg.base.name }}</span
+                    >(<args :args="arg.arguments" />)</em
+                >
             </template>
-            <arg :arg="arg" v-else/>
-            <em v-if="i < call.args.length - 1">,</em>
+            <arg :arg="arg" v-else />
+            <em v-if="i < call.args.length - 1" class="u-call-op">,</em>
         </span>
         <span>)</span>
     </div>
 </template>
 
 <script>
-import args from '@/components/skill_item_call_args.vue'
-import arg from '@/components/skill_item_call_arg.vue'
+import args from "@/components/skill_item_call_args.vue";
+import arg from "@/components/skill_item_call_arg.vue";
 export default {
     name: "skill_item_call",
     props: ["data"],
@@ -34,13 +37,16 @@ export default {
     mounted: function() {},
     components: {
         args,
-        arg
+        arg,
     },
 };
 </script>
 
 <style scoped lang="less">
-    .u-fn{
-        color:#93f;
-    }
+.u-fn {
+    color: #93f;
+}
+.u-call-op {
+    color: #f00;
+}
 </style>
--- src/components/skill_item_call_arg.vue
+++ src/components/skill_item_call_arg.vue
@@ -2,8 +2,8 @@
     <span class="u-call-arg">
         <template v-if="arg.type == 'MemberExpression'">
             <em
-                >{{ arg.base.name }}<b>{{ arg.indexer
-                }}</b>{{ arg.identifier.name }}</em
+                >{{ arg.base.name }}<b>{{ arg.indexer }}</b
+                >{{ arg.identifier.name }}</em
             >
         </template>
         <template v-else-if="arg.type == 'NumericLiteral'">
@@ -17,17 +17,18 @@
         </template>
         <template v-else-if="arg.type == 'BinaryExpression'">
             <em
-                >{{ arg.left.raw || arg.left.name }}<b>{{ arg.operator
-                }}</b>{{ arg.right.raw || arg.right.name }}</em
+                >{{ arg.left.raw || arg.left.name
+                }}<b class="u-call-op">{{ arg.operator }}</b
+                >{{ arg.right.raw || arg.right.name }}</em
             >
         </template>
         <template v-else-if="arg.type == 'UnaryExpression'">
             <em
-                ><b>{{ arg.operator
-                }}</b>{{ arg.argument.raw || arg.argument.name }}</em
+                ><b>{{ arg.operator }}</b
+                >{{ arg.argument.raw || arg.argument.name }}</em
             >
         </template>
-        <template v-else>{{ arg }}</template>
+        <span v-else class="u-call-op">{{ arg }}</span>
     </span>
 </template>
 
@@ -46,15 +47,16 @@ export default {
 </script>
 
 <style scoped lang="less">
-    .u-call-arg{
-        em,b{
-            .mr(0) !important;
-        }
-        em{
-            color:#49c10f;
-        }
-        b{
-            color:#f00;
-        }
+.u-call-arg {
+    em,
+    b {
+        .mr(0) !important;
     }
+    em {
+        color: #49c10f;
+    }
+}
+.u-call-op {
+    color: #f00;
+}
 </style>

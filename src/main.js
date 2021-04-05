8bab321eca9fa727fe43317ced2ff7256b7cef42
iRuxu <rx6@qq.com>
2021-04-05 13:38:53 +0800

    fix copy

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import Vue from "vue";
 // Vue.config.productionTip = false;
 import ElementUI from "element-ui";
 Vue.use(ElementUI);
-// import VueClipboard from 'vue-clipboard2'
-// Vue.use(VueClipboard)
+import VueClipboard from 'vue-clipboard2'
+Vue.use(VueClipboard)
 
 // 通用UI模块
 import JX3BOX_UI from '@jx3box/jx3box-common-ui'

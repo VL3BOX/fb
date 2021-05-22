19b371cde549bb4e86b562e313a31b0b03852a04
iRuxu <rx6@qq.com>
2021-05-22 14:03:44 +0800

    updatee

--- src/views/Attr.vue
+++ src/views/Attr.vue
@@ -4,14 +4,21 @@
         <div class="u-desc">穿透属性会将通用减伤效果降低100%，无视格挡伤害类效果。依旧可以受到属性减伤、小部分伤害吸收盾、内外功防御以及免死效果影响。</div>
         <ul class="u-content">
             <li v-for="(item,i) in chuantou" :key="i">
-                <a :href="item.link" target="_blank"><img :src="item.icon | iconLink">{{item.label}}</a>
+                <a :href="item.link" target="_blank"><img :src="item.icon | iconLink">{{item.label}}<span v-if="item.color">{{item.color}}</span></a>
             </li>
         </ul>
         <el-divider content-position="left">穿刺</el-divider>
         <div class="u-desc">穿刺伤害会在伤害作用玩家之前主要删除/无视无敌类型的技能，但是依旧可以受到通用减伤、属性减伤、部分分担伤害、伤害吸收盾、内外功防御以及免死效果影响。</div>
         <ul class="u-content">
             <li v-for="(item,i) in chuanci" :key="i">
-                <a :href="item.link" target="_blank"><img :src="item.icon | iconLink">{{item.label}}</a>
+                <a :href="item.link" target="_blank"><img :src="item.icon | iconLink">{{item.label}}<span v-if="item.color">{{item.color}}</span></a>
+            </li>
+        </ul>
+        <el-divider content-position="left">贯体</el-divider>
+        <div class="u-desc">可按百分比治疗目标的技能，在有较高禁疗下时很重要。</div>
+        <ul class="u-content">
+            <li v-for="(item,i) in guanti" :key="i">
+                <a :href="item.link" target="_blank"><img :src="item.icon | iconLink">{{item.label}}<span v-if="item.color">{{item.color}}</span></a>
             </li>
         </ul>
     </div>
@@ -29,6 +36,7 @@ export default {
             loading: false,
             chuantou: "",
             chuanci: "",
+            guanti : ""
         };
     },
     filters : {
@@ -38,7 +46,7 @@ export default {
         loadData: function () {
             this.loading = true;
             getMenuGroups({
-                names: ["chuantou", "chuanci"],
+                names: ["chuantou", "chuanci","guanti"],
             })
                 .then((res) => {
                     let data = res.data.data.data;

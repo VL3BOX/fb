57af39317bb43d01ec6f3306d540a2d693de9454
iRuxu <17229398@qq.com>
2022-07-26 17:23:03 +0800

    fix:story route query

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -7,10 +7,16 @@
                     <el-descriptions :column="3" border>
                         <el-descriptions-item label="名称">{{ item.OtherName }}</el-descriptions-item>
                         <el-descriptions-item label="地图ID">{{ item.MapID }}</el-descriptions-item>
-                        <el-descriptions-item label="资料片">Level.{{ item.DivideLevel }}／{{ item.DivideName }}／{{ item.VersionName }}</el-descriptions-item>
+                        <el-descriptions-item label="资料片"
+                            >Level.{{ item.DivideLevel }}／{{ item.DivideName }}／{{
+                                item.VersionName
+                            }}</el-descriptions-item
+                        >
                         <el-descriptions-item label="最低等级要求">{{ item.MinLevel }}</el-descriptions-item>
                         <el-descriptions-item label="入口" :span="2">{{ item.EnterWay }}</el-descriptions-item>
-                        <el-descriptions-item label="简介">{{ item.Introduction | formartIntroduction }}</el-descriptions-item>
+                        <el-descriptions-item label="简介">{{
+                            item.Introduction | formatIntroduction
+                        }}</el-descriptions-item>
                     </el-descriptions>
                 </div>
                 <div class="m-story-boss">
@@ -22,9 +28,17 @@
                                 {{ boss.BOSS }}
                             </h5>
                             <p class="u-boss-detail" v-html="boss.Introduce"></p>
-                            <el-button class="u-boss-more" type="primary" plain @click="loadSkills(boss)" size="mini" icon="el-icon-key">{{
-                                !skill_list[boss.NPCID] ? "查看技能" : skill_status[boss.NPCID] ? "收起" : "展开"
-                            }}</el-button>
+                            <el-button
+                                class="u-boss-more"
+                                type="primary"
+                                plain
+                                @click="loadSkills(boss)"
+                                size="mini"
+                                icon="el-icon-key"
+                                >{{
+                                    !skill_list[boss.NPCID] ? "查看技能" : skill_status[boss.NPCID] ? "收起" : "展开"
+                                }}</el-button
+                            >
                             <div class="u-boss-skills" v-show="skill_status[boss.NPCID]">
                                 <template v-if="skill_list[boss.NPCID]">
                                     <div class="u-skill" v-for="skill in skill_list[boss.NPCID]" :key="skill.idkey">
@@ -32,7 +46,9 @@
                                         <div class="u-skill-desc">{{ skill.Desc }}</div>
                                     </div>
                                 </template>
-                                <div class="u-skill-null" v-else><i class="el-icon-warning-outline"></i> 没有相关信息</div>
+                                <div class="u-skill-null" v-else>
+                                    <i class="el-icon-warning-outline"></i> 没有相关信息
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -47,11 +63,10 @@
 import { __ossMirror } from "@jx3box/jx3box-common/data/jx3box";
 import { getInfo, getBoss, getBossSkills } from "@/service/getStory.js";
 import { extractTextContent } from "@jx3box/jx3box-common/js/utils";
-import compact from "lodash/compact";
 export default {
     name: "Story",
     props: [],
-    data: function() {
+    data: function () {
         return {
             loading: false,
             map_id: "",
@@ -64,30 +79,32 @@ export default {
         };
     },
     computed: {
-        fb: function() {
+        fb: function () {
             return this.$route.query.fb_name || this.$store.state.default_fb;
         },
-        client: function() {
+        client: function () {
             return this.$store.state.client;
         },
-        map_cover: function() {
+        map_cover: function () {
             return __ossMirror + "pic/map/map_" + this.mapid + "_0.png";
         },
     },
     methods: {
-        loadData: function() {
+        loadData: function () {
             this.loading = true;
             getInfo(this.fb, this.client)
                 .then((res) => {
                     this.data = res?.data || [];
-                    this.map_id = String(this.data?.[0]?.["MapID"]);
+                    if(!this.map_id){
+                        this.map_id = String(this.data?.[0]?.["MapID"]);
+                    }
                     return this.map_id;
                 })
                 .finally(() => {
                     this.loading = false;
                 });
         },
-        loadSkills: function(boss) {
+        loadSkills: function (boss) {
             let npc_id = boss?.NPCID;
             if (this.skill_list[npc_id]) {
                 this.skill_status[npc_id] = !this.skill_status[npc_id];
@@ -113,10 +130,10 @@ export default {
         },
     },
     watch: {
-        fb: function() {
+        fb: function () {
             this.loadData();
         },
-        map_id: function(id) {
+        map_id: function (id) {
             if (id) {
                 this.loading = true;
                 getBoss(id, this.client)
@@ -128,13 +145,21 @@ export default {
                     });
             }
         },
+        "$route.query.map_id": {
+            immediate: true,
+            handler: function (id) {
+                if (id) {
+                    this.map_id = id;
+                }
+            },
+        },
     },
     filters: {
-        formartIntroduction: function(str) {
+        formatIntroduction: function (str) {
             return extractTextContent(str)?.[0]?.text;
         },
     },
-    mounted: function() {
+    mounted: function () {
         this.loadData();
     },
 };

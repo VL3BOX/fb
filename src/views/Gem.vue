61a2aa49622b17e994cea4b715ee885f1ea8bb95
X3ZvaWQ <x3zvawq@gmail.com>
2022-09-01 05:07:56 +0800

    fix: gem desc use new componenet

--- src/views/Gem.vue
+++ src/views/Gem.vue
@@ -9,15 +9,15 @@
             <div class="m-gem-item" v-for="(item, i) in data" :key="i">
                 <img class="u-icon" :src="item.IconID | iconLink" />
                 <span class="u-title">
-                    <a :href="'/item' + item.Link"
-                    target="_blank"
-                    draggable="false">{{ item.Name }}</a>
-                    <i class="el-icon-copy-document"
+                    <a :href="'/item' + item.Link" target="_blank" draggable="false">{{ item.Name }}</a>
+                    <i
+                        class="el-icon-copy-document"
                         v-clipboard:copy="item.Name"
                         v-clipboard:success="onCopy"
-                        v-clipboard:error="onCopyError"></i>
+                        v-clipboard:error="onCopyError"
+                    ></i>
                 </span>
-                <span class="u-desc" v-html="item.DescHtml"></span>
+                <game-text class="u-desc" :text="item.Desc"></game-text>
                 <!-- <span class="u-drops">
                     <span
                         class="u-drop"
@@ -31,7 +31,16 @@
             </div>
         </div>
         <el-alert v-else class="m-archive-null" title="没有找到相关条目" type="info" center show-icon> </el-alert>
-        <el-button class="m-archive-more" :class="{ show: hasNextPage }" type="primary" :loading="loading" @click="appendPage(++page)" icon="el-icon-arrow-down" v-show="hasNextPage">加载更多</el-button>
+        <el-button
+            class="m-archive-more"
+            :class="{ show: hasNextPage }"
+            type="primary"
+            :loading="loading"
+            @click="appendPage(++page)"
+            icon="el-icon-arrow-down"
+            v-show="hasNextPage"
+            >加载更多</el-button
+        >
         <el-pagination
             class="m-archive-pages"
             background
@@ -47,13 +56,17 @@
 </template>
 
 <script>
+import GameText from "@jx3box/jx3box-editor/src/GameText.vue"
 import { getGemList } from "../service/getGem";
 import { __ossMirror, __iconPath, __ossRoot } from "@jx3box/jx3box-common/data/jx3box";
 import { iconLink, getLink } from "@jx3box/jx3box-common/js/utils";
 export default {
     name: "Gem",
     props: [],
-    data: function() {
+    components: {
+        GameText,
+    },
+    data: function () {
         return {
             data: [],
             total: 0,
@@ -66,19 +79,19 @@ export default {
         };
     },
     computed: {
-        hasNextPage: function() {
+        hasNextPage: function () {
             return this.total > 1 && this.page < this.pages;
         },
-        fb: function() {
+        fb: function () {
             return this.$route.query.fb_name || this.$store.state.default_fb;
         },
-        zlp: function() {
+        zlp: function () {
             return this.$route.query.fb_zlp || this.$store.state.default_zlp;
         },
-        client: function() {
+        client: function () {
             return this.$store.state.client || "std";
         },
-        params: function() {
+        params: function () {
             return {
                 dungeon: this.fb,
                 page: this.page,
@@ -89,7 +102,7 @@ export default {
     },
     filters: { iconLink },
     methods: {
-        loadPosts: function() {
+        loadPosts: function () {
             this.loading = true;
             getGemList(this.params)
                 .then((res) => {
@@ -112,47 +125,47 @@ export default {
                     this.loading = false;
                 });
         },
-        changePage: function() {
+        changePage: function () {
             window.scrollTo(0, 0);
             this.appendMode = false;
         },
-        appendPage: function(i) {
+        appendPage: function (i) {
             this.appendMode = true;
         },
-        getDesc: function(str) {
+        getDesc: function (str) {
             const RE = /使用(.*?)装备。/;
             let result = RE.exec(str);
             return result[0];
         },
-        getDrops: function(str) {
+        getDrops: function (str) {
             const RE = /\[(.*?)\]/g;
             let result = str.match(RE) || [];
             return result;
         },
         getLink,
-        onCopy: function() {
+        onCopy: function () {
             this.$notify({
-				title: "复制成功",
-				message: "复制物品名称成功",
-				type: "success",
-			});
+                title: "复制成功",
+                message: "复制物品名称成功",
+                type: "success",
+            });
         },
-        onCopyError: function() {
+        onCopyError: function () {
             this.$notify.error({
-				title: "复制失败",
-				message: "请手动复制",
-			});
-        }
+                title: "复制失败",
+                message: "请手动复制",
+            });
+        },
     },
     watch: {
         params: {
             deep: true,
-            handler: function() {
+            handler: function () {
                 this.loadPosts();
             },
         },
     },
-    created: function() {
+    created: function () {
         this.loadPosts();
     },
 };

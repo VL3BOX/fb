9cc7e8b1a7b6fcf3f6f17a40a5fc8c5b5e2fc270
iRuxu <rx6@qq.com>
2023-04-01 19:03:05 +0800

    fix:fb3

--- src/service/team.js
+++ src/service/team.js
@@ -25,3 +25,26 @@ export function getTeams(params) {
 }
 
 
+
+import axios from "axios";
+import { __next } from "@jx3box/jx3box-common/data/jx3box.json";
+const API = __next + "api/fb/rank/";
+
+export function getDateRank(id) {
+    let limit = 100;
+    if (id == 7529) limit = 66;
+    return axios.get(API + id, {
+        params: {
+            pageSize: limit,
+        },
+    });
+}
+
+export function getMiniRank(id) {
+    return axios.get(API + id, {
+        params: {
+            pageSize: 10,
+        },
+    });
+}
+

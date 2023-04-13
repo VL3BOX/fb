d4e291aaed62453321409a49142ccd8338f91632
Kaviilee <jiaweilee95@126.com>
2023-04-14 00:29:31 +0800

    update

--- src/service/lua.js
+++ src/service/lua.js
@@ -1,14 +1,14 @@
 import { $lua } from "@jx3box/jx3box-common/js/https";
 
 function getLuaIndex(fbname, client = "std") {
-    return $lua({ mute: true }).get(`/lua/fb/src/${fbname}/index.json`, {
+    return $lua({ mute: true }).get(`/api/lua/fb/src/${fbname}/index.json`, {
         params: {
             client: client,
         },
     });
 }
 function getLua(fbname, filename, client = "std") {
-    let _path = encodeURIComponent(`lua/fb/src/${fbname}/${filename}`);
+    let _path = encodeURIComponent(`/api/lua/fb/src/${fbname}/${filename}`);
     return $lua().get(_path, {
         params: {
             client: client,

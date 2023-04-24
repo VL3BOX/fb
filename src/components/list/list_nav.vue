dca12319a8983d80d8acad3da107cd0d991ca72b
iRuxu <rx6@qq.com>
2023-04-24 23:31:25 +0800

    ci:sync common

--- src/components/list/list_nav.vue
+++ src/components/list/list_nav.vue
@@ -5,7 +5,7 @@
                 <img class="u-img" :src="getMap(fbDetail.icon)" @click="onMapBgClick" />
                 <div class="u-name">{{ fbName }}</div>
             </div>
-            <div class="m-nav-search" @click.stop >
+            <div class="m-nav-search" @click.stop>
                 <!-- 下拉框 -->
                 <el-select
                     v-model="search"
@@ -67,36 +67,6 @@
 
         <div class="m-nav-app">
             <h5 class="u-title">在线应用</h5>
-            <!-- <a href="/jdt" target="_blank">
-                <img class="u-icon" :src="getAppIcon('jdt')" />
-                <span>秘境百强</span>
-                <em>JX3 Dungeon Top100</em>
-            </a> -->
-            <a href="/team" target="_blank">
-                <img class="u-icon" :src="getAppIcon('team')" />
-                <span>团队平台</span>
-                <em>Team Platform</em>
-            </a>
-            <a href="/jcl" target="_blank">
-                <img class="u-icon" :src="getAppIcon('jcl')" />
-                <span>日志分析</span>
-                <em>JX3 Combat Log</em>
-            </a>
-            <a href="/battle" target="_blank">
-                <img class="u-icon" :src="getAppIcon('battle')" />
-                <span>战斗统计</span>
-                <em>Battle Statistics</em>
-            </a>
-            <!-- <a href="/jcl" target="_blank" class="disabled" @click.prevent>
-                <img class="u-icon" src="@/assets/img/timeline.svg" />
-                <span>时间轴</span>
-                <em>Boss Timeline</em>
-            </a> -->
-            <!-- <a href="/dbm" target="_blank" class="disabled" @click.prevent>
-                <img class="u-icon" :src="getAppIcon('dbm')" />
-                <span>数据构建</span>
-                <em>DBM Data Builder</em>
-            </a> -->
             <a href="/fb/bahuang" target="_blank" v-if="client == 'origin'">
                 <img class="u-icon" :src="getAppIcon('bhhj')" />
                 <span>八荒衡鉴</span>
@@ -107,6 +77,36 @@
                 <span>百战查询</span>
                 <em>Bai Zhan</em>
             </a>
+            <a href="/dbm" target="_blank">
+                <img class="u-icon" :src="getAppIcon('dbm')" />
+                <span>数据构建</span>
+                <em>DBM Data Builder</em>
+            </a>
+            <a href="/battle" target="_blank">
+                <img class="u-icon" :src="getAppIcon('battle')" />
+                <span>战斗统计</span>
+                <em>Battle Statistics</em>
+            </a>
+            <a href="/jcl" target="_blank">
+                <img class="u-icon" :src="getAppIcon('jcl')" />
+                <span>日志分析</span>
+                <em>JX3 Combat Log</em>
+            </a>
+            <a href="/jdt" target="_blank">
+                <img class="u-icon" :src="getAppIcon('jdt')" />
+                <span>秘境百强</span>
+                <em>JX3 Dungeon Top100</em>
+            </a>
+            <a href="/team" target="_blank">
+                <img class="u-icon" :src="getAppIcon('team')" />
+                <span>团队平台</span>
+                <em>Team Platform</em>
+            </a>
+            <!-- <a href="/jcl" target="_blank" class="disabled" @click.prevent>
+                <img class="u-icon" src="@/assets/img/timeline.svg" />
+                <span>时间轴</span>
+                <em>Boss Timeline</em>
+            </a> -->
         </div>
     </div>
 </template>

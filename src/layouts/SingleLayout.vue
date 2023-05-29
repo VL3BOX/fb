6bc85ca4d91371fec684e3338ea0b256d428cfda
Kaviilee <jiaweilee95@126.com>
2023-05-29 17:15:51 +0800

    feat: update pkg & comment

--- src/layouts/SingleLayout.vue
+++ src/layouts/SingleLayout.vue
@@ -8,7 +8,7 @@
         </LeftSidebar>
         <Main :withoutRight="false">
             <slot></slot>
-            <RightSidebar>
+            <RightSidebar :show-toggle="true">
                 <Side :id="id" class="m-extend" />
             </RightSidebar>
             <Footer></Footer>

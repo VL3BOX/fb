ae77224ceb10a9efeb59edc56178acffd55b99c6
Kaviilee <jiaweilee95@126.com>
2023-05-30 22:24:12 +0800

    feat: update pkg

--- src/layouts/SingleLayout.vue
+++ src/layouts/SingleLayout.vue
@@ -2,6 +2,9 @@
     <div id="app">
         <Header></Header>
         <Breadcrumb name="副本专栏" slug="fb" :publishEnable="true" :feedbackEnable="true" :adminEnable="true">
+            <template #op-prepend>
+                <AdminDirectMessage :user-id="user_id" :sourceId="post.ID" :sourceType="post.post_type"></AdminDirectMessage>
+            </template>
         </Breadcrumb>
         <LeftSidebar :uid="user_id">
             <Nav :id="id" class="m-nav" />
@@ -20,6 +23,7 @@
 import Nav from "@/components/single/single_nav.vue";
 import Side from "@/components/single/single_side.vue";
 import { getAppIcon, getAppID } from "@jx3box/jx3box-common/js/utils";
+import AdminDirectMessage from "@jx3box/jx3box-common-ui/src/bread/AdminDirectMessage.vue"
 export default {
     name: "SingleLayout",
     props: [],
@@ -32,11 +36,15 @@ export default {
         user_id: function () {
             return this.$store.state.user_id;
         },
+        post: function (){
+            return this.$store.state.post;
+        }
     },
     methods: { getAppIcon },
     components: {
         Nav,
         Side,
+        AdminDirectMessage
     },
 };
 </script>

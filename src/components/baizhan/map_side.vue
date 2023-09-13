4604064dce6389f151ae369eb04d5ef749102039
fifththirteen <570205219@qq.com>
2023-09-13 11:19:35 +0800

    fix:副本右侧边栏百战地图组件更换

--- src/components/baizhan/map_side.vue
+++ src/components/baizhan/map_side.vue
@@ -15,7 +15,8 @@
                 </a>
             </div>
             <div class="u-time">最后更新时间: {{ update_time }}</div>
-            <div class="u-tabs" v-if="maps.length">
+            <BMap mode="list"></BMap>
+            <!-- <div class="u-tabs" v-if="maps.length">
                 <div
                     class="u-tab"
                     :class="activeIndex === i && 'is-active'"
@@ -36,7 +37,6 @@
                     @click="toMap(index)"
                 >
                     <div class="u-item-left">
-                        <!-- <div class="u-index">{{ (activeIndex - 1) * per + index + 1 }}</div> -->
                         <div class="u-index">{{ index + 1 }}</div>
                         <div class="u-avatar">
                             <img :src="item.bossAvatar" :alt="item.bossName" />
@@ -47,100 +47,40 @@
                         <img :src="iconLink(item.effect.dwIconID)" />
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
 import { moment } from "@jx3box/jx3box-common/js/moment";
-import { cloneDeep } from "lodash";
-import { iconLink } from "@jx3box/jx3box-common/js/utils";
+import BMap from "@jx3box/jx3box-bmap/src/components/BMap.vue";
 export default {
     name: "MapSide",
+    components: {
+        BMap,
+    },
     data() {
         return {
             loading: false,
-            base: 3,
-            activeIndex: 1,
             qq: "785597424",
         };
     },
     computed: {
-        ...mapState({
-            bosses: (state) => state.baizhan.bosses,
-            effects: (state) => state.baizhan.effects,
-            maps: (state) => state.baizhan.maps,
-        }),
         update_moment() {
             return moment(this.$store.state.baizhan.map.updated_at);
         },
         update_time() {
             return this.update_moment.format("YYYY/MM/DD HH:mm:ss");
         },
-        per() {
-            return this.maps.length / this.base;
-        },
-        mapList() {
-            const list = [];
-            let per = this.per;
-            const maps = cloneDeep(this.maps);
-            for (let i = 0; i < this.base; i++) {
-                list.push(maps.slice(i * per, (i + 1) * per));
-            }
-            return list;
-        },
         client() {
             return this.$store.state.client;
         },
     },
-    watch: {
-        activeIndex(index) {
-            const baseIndex = this.maps.length / this.base;
-            const refIndex = (index - 1) * baseIndex ? (index - 1) * baseIndex - 1 : 0;
-            this.$refs.mapItem[refIndex].scrollIntoView({
-                behavior: "smooth",
-                block: refIndex ? "start" : "center",
-            });
-        },
-    },
     methods: {
-        ...mapActions({
-            loadBosses: "baizhan/loadBosses",
-            loadSkills: "baizhan/loadSkills",
-            loadEffects: "baizhan/loadEffects",
-            loadMap: "baizhan/loadMap",
-        }),
-        iconLink,
-        getSpecialStyle(i) {
-            // const index = (this.activeIndex - 1) * this.per + i + 1;
-            const index = i + 1;
-            return !(index % 10) ? "is-special" : "";
-        },
-        toMap(i) {
+        toMap() {
             let url = `${location.origin}/fb/baizhan`;
-            if (!isNaN(i)) {
-                url += `?floor=${i + 1}`;
-            }
             window.open(url, "_blank");
-        },
-        load() {
-            const proArr = [];
-
-            const bossPro = this.loadBosses();
-            proArr.push(bossPro);
-
-            const skillPro = this.loadSkills();
-            proArr.push(skillPro);
-
-            const effectPro = this.loadEffects();
-            proArr.push(effectPro);
-            this.loading = true;
-            Promise.allSettled(proArr).then(() => {
-                this.loadMap();
-                this.loading = false;
-            });
         },
         onQQClick() {
             navigator.clipboard.writeText(this.qq).then(() => {
@@ -152,9 +92,7 @@ export default {
             });
         },
     },
-    mounted() {
-        this.load();
-    },
+    mounted() {},
 };
 </script>
 

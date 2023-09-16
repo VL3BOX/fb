b976497b37812a1c6573207c972529b0e450b3e0
Kaviilee <jiaweilee95@126.com>
2023-09-16 12:52:30 +0800

    chore: update pkg

--- src/layouts/SingleLayout.vue
+++ src/layouts/SingleLayout.vue
@@ -5,6 +5,11 @@
             <template #op-prepend>
                 <AdminDirectMessage :user-id="user_id" :sourceId="post.ID" :sourceType="post.post_type"></AdminDirectMessage>
             </template>
+            <template #title>
+                <span>
+                    {{ title }}
+                </span>
+            </template>
         </Breadcrumb>
         <LeftSidebar :uid="user_id">
             <Nav :id="id" class="m-nav" />
@@ -38,6 +43,9 @@ export default {
         },
         post: function (){
             return this.$store.state.post;
+        },
+        title() {
+            return this.post.post_title || document.title;
         }
     },
     methods: { getAppIcon },
